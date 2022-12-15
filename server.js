268c67f55f89935e9476e34149c4d544646e0e11
SoulDevt <souleyboyz@gmail.com>
2022-12-15 12:36:24 +0100

    config socket emit,disconnection,message

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
+
 
 // config statics folders
 app.use(express.static(path.join(__dirname, 'public')))
 
+//run when a client connects
+io.on('connection', socket => {
+   console.log('new connection established')
+
+
+   //Welcome current user
+   socket.emit("message", 'Welcome to Chat')
+
+   //Broadcasting when someone connects
+   socket.broadcast.emit('message', 'new user joined the chat');
+
+   //Runs when client disconnects
+   socket.on('disconnect', () => {
+      io.emit('message', "a user left the chat")
+   })
+
+})
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
 });
